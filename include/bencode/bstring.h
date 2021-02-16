0920378967c73215cafc290c857a5e42d58eade3
Ferdinand <ferdinandvwalree@gmail.com>
2021-02-16 00:04:57 +0000

    Added string constructor for bstring

--- include/bencode/bstring.h
+++ include/bencode/bstring.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "btypes.h"
+#include <algorithm>
 #include <string>
 #include <sstream>
 
@@ -9,13 +10,17 @@ using namespace std;
 namespace bencode {
 
 	class bstring {
-		vector<char> s;
+		vector<char> v;
 	public:
-		bstring(vector<char> ss) : s(ss) {};
+		bstring(vector<char> vs) : v(vs) {};
+		bstring(string s) {
+			v.reserve(s.size());
+			std::copy(s.begin(),s.end(),v.begin());
+		 }
 
 		string display_type();
-		vector<char> value() const { return s; }
-		string to_string() const { return string(s.begin(),s.end()); }
+		vector<char> value() const { return v; }
+		string to_string() const { return string(v.begin(),v.end()); }
 
 		friend ostream& operator<<(ostream& os, bstring& bs);
 		friend bool operator<(bstring b1,bstring b2);
